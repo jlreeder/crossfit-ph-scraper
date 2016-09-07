62ba6ed8bd0d0ef748a540eb62b5f01d2d2139b3
Jason Reeder <info@jasonreeder.com>
2016-09-07 14:04:37 -0700

    Add date specification

--- crossfit_ph_scraper.py
+++ crossfit_ph_scraper.py
@@ -1,12 +1,20 @@
 import urllib.request
 from bs4 import BeautifulSoup
 
-def get_content():
+def format_date():
     """
-    Get the content of the WOD blog
+    TODO: Format date for wod blog
+    """
+    pass
+
+def get_content(date):
+    """
+    Get the content of the WOD blog for a given date
     """
 
-    crossfit_ph_url = "http://crossfitph.com/wod-blog/"
+    base_url = "http://crossfitph.com/"
+    crossfit_ph_url = base_url + date
+
     req = urllib.request.Request(crossfit_ph_url,
                                  headers={'User-Agent': 'Mozilla/5.0'})
     response = urllib.request.urlopen(req)
@@ -14,5 +22,3 @@ def get_content():
     soup = BeautifulSoup(page, "html.parser")
 
     return soup
-
-# get_content(crossfit_ph_url)
--- crossfit_ph_scraper_tests.py
+++ crossfit_ph_scraper_tests.py
@@ -3,8 +3,8 @@ from crossfit_ph_scraper import *
 
 class Test(unittest.TestCase):
 
-    def test_retrieve_blog(self):
-        self.assertTrue(get_content())
+    def test_get_content_for_date(self):
+        self.assertTrue(get_content("2016/09/06/wod-wednesday-sept-7/"))
 
 if __name__ == '__main__':
     unittest.main()
