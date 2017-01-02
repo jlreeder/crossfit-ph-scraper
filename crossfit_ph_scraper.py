d6a163f6ea6ad40d2851b608fc4d971763b79dbe
Jason Reeder <info@jasonreeder.com>
2017-01-01 18:38:38 -0800

    Detect headers by ending
    There are many different headers used. Instead of hard coding each one, they can
    be detected by final colons.

--- crossfit_ph_scraper.py
+++ crossfit_ph_scraper.py
@@ -92,16 +92,12 @@ def format_content(page):
     lines = [line.strip() for line in lines if line]
     lines = lines[4:]
 
-    # Format
-    HEADERS = ["Strength A:", "Strength B:", "Strength:", "WOD:", "Skill:",
-               "Warm:", "Finisher:", "9:30 and 10:30 am Regular CrossFit",
-               "8:30 am Lite N Tite", "Benchmark WOD:"]
-
     # Add contents as bullets
     wod_text = ""
     for line in lines:
         prefix = "  - "
-        if line in HEADERS:
+        # Do not add indent for headers
+        if line.endswith(":") and not line.startswith("Lv"):
             prefix = "- "
         line = prefix + line
         wod_text += line + "\n"
